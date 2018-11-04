6566c138fa428b825da4711ce3bbbfbac8b0c6db
Stanislav  Kosatkin <stanislavkosatkin@MacBook-Pro-Stanislav.local>
2018-11-04 10:13:56 +0200

    move from to php-7.2

--- app.dockerfile
+++ app.dockerfile
@@ -1,9 +1,9 @@
-FROM php:7.0.4-fpm
+FROM php:7.2.11-fpm
 
 RUN apt-get update && apt-get install -y libmcrypt-dev \
     mysql-client libmagickwand-dev --no-install-recommends \
     && pecl install imagick \
     && docker-php-ext-enable imagick \
-    && docker-php-ext-install mcrypt pdo_mysql \
+    && docker-php-ext-install pdo_mysql \
     && pecl install xdebug \
     && docker-php-ext-enable xdebug
--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,13 @@ use App\Article;
 |
 */
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{article}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update');
-Route::delete('articles/{article}', 'ArticleController@delete');
+Route::group(['middleware' => 'auth:api'], function() {
+	Route::get('articles', 'ArticleController@index');
+	Route::get('articles/{article}', 'ArticleController@show');
+	Route::post('articles', 'ArticleController@store');
+	Route::put('articles/{article}', 'ArticleController@update');
+	Route::delete('articles/{article}', 'ArticleController@delete');
+});
 
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
